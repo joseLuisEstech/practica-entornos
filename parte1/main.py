2a9f242c4102d47426ca293113a01a57afe6dc94
Jose Luis <josepayoyo2003@gmail.com>
2024-12-13 18:47:24 +0100

    Terminada parte 1

---
+++ parte1/main.py
@@ -0,0 +1,9 @@
+a = 5
+b = 3
+
+if a > b:
+    print(f"La variable 'a' ({a}) es mayor que 'b' ({b}).")
+elif a < b:
+    print(f"La variable 'b' ({b}) es mayor que 'a' ({a}).")
+else:
+    print(f"Ambas variables son iguales: a = {a}, b = {b}.")
